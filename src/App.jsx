33928dde5ff286efeb28a9cb28d97cbadc9ea205
Wilson P <wilsonprajnawira345@gmail.com>
2025-05-19 14:33:12 +0700

    Initial File

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-xl font-bold text-red-500'>halo</h1>
+    </>
+  )
+}
+
+export default App
