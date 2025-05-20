17c8aa17e18f16e9b3d7b2bec0c9e6fe5738872e
Wilson P <wilsonprajnawira345@gmail.com>
2025-05-20 20:01:30 +0700

    Struktur

---
+++ src/components/WelcomeSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WelcomeSection = () => {
+  return (
+    <div>
+      <h1 className='text-xl font-bold text-red-500'>halo</h1>
+    </div>
+  )
+}
+
+export default WelcomeSection
